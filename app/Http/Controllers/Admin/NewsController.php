f2ecf729a541e67f72289c017796649d8c059b54
片岡利紗 <risakataoka6@gmail.com>
2019-01-12 21:51:20 +0900

    firstcommit

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsController extends Controller
+{
+    //public function=Action
+    public function add(){
+      return view('admin.news.create');
+    }
+}
+
+
+
+
+
+
+
+
+?>
