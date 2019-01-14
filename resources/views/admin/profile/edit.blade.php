6b0691436ed7bbb748a0c8e11dd3e68d1f47b609
片岡利紗 <risakataoka6@gmail.com>
2019-01-14 11:27:43 +0900

    firstcommit

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.admin')
 
-        <title>PHP/Laravel</title>
-    </head>
-    <body>
-        <h1>PHP/Laravel 11 – [Mac編]ControllerとViewが連携できるようにしよう</h1>
-    </body>
-</html>
+
+{{-- profile.blade.phpの@yield('title')に'私のプロフィール'を埋め込む --}}
+@section('title', 'MyProfile')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
